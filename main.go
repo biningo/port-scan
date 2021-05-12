a19babdce47d0e22f6074e5a8f9f90ec0eab969f
biningo <biningo.cn@gmail.com>
2021-05-12 21:07:27 +0800

    feat: add some options

--- main.go
+++ main.go
@@ -1,25 +1,28 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"net"
 	"sort"
 	"sync"
+	"time"
 )
 
 const (
-	MAX_PORT   = 1 << 16
 	MAX_WORKER = 100
 )
 
 var IP = "localhost"
 var wg sync.WaitGroup
+var timeout = 0
 
 func worker(ports <-chan int, result chan<- int) {
 	defer wg.Done()
 	for port := range ports {
 		address := fmt.Sprintf("%s:%d", IP, port)
-		if _, err := net.Dial("tcp", address); err == nil {
+		if _, err := net.DialTimeout("tcp", address, time.Second*time.Duration(timeout)); err == nil {
 			result <- port
 		} else {
 			result <- -1
@@ -28,6 +31,17 @@ func worker(ports <-chan int, result chan<- int) {
 }
 
 func main() {
+	start := 0
+	end := 65535
+	flag.IntVar(&start, "start", 0, "start")
+	flag.IntVar(&end, "end", 65535, "end")
+	flag.StringVar(&IP, "ip", "localhost", "IP")
+	flag.IntVar(&timeout, "timeout", 3, "timeout(second)")
+	flag.Parse()
+
+	if start < 0 || start > 65535 || end < 0 || end > 65535 {
+		panic(errors.New("port range error"))
+	}
 	ports := make(chan int, MAX_WORKER)
 	result := make(chan int)
 	for i := 0; i < MAX_WORKER; i++ {
@@ -35,13 +49,13 @@ func main() {
 		go worker(ports, result)
 	}
 	go func() {
-		for port := 0; port < MAX_PORT; port++ {
+		for port := start; port <= end; port++ {
 			ports <- port
 		}
 		close(ports)
 	}()
 	openPorts := []int{}
-	for i := 0; i < MAX_PORT; i++ {
+	for i := start; i <= end; i++ {
 		port := <-result
 		if port != -1 {
 			openPorts = append(openPorts, port)
